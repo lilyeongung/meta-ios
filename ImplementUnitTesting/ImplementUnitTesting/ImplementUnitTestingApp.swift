d6c5b64f6d4717c61d8b36114a0f5270fb901faa
lil yeongung <148127804+lilyeongung@users.noreply.github.com>
2024-05-22 12:59:59 -0700

    unit testing

---
+++ ImplementUnitTesting/ImplementUnitTesting/ImplementUnitTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImplementUnitTestingApp.swift
+//  ImplementUnitTesting
+//
+//  Created by Andrew Park on 5/22/24.
+//
+
+import SwiftUI
+
+@main
+struct ImplementUnitTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
